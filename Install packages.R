dd5dd565dd3dea8ceaf2c811bbb0ccaa481d4041
Sara López Fernández (Student) <salz0002@stud.slu.se>
2023-04-28 14:00:52 +0200

    I have add more packages

--- Install packages.R
+++ Install packages.R
@@ -1,4 +1,14 @@
 ##Packages to install before the adventure
-
+#richness analyses
 install.packages("vegan")
-install.packages("physeq")
+install.packages("tidyverse")
+
+#CA, ordenation analyses
+install.packages("factoextra")
+install.packages("ca")
+install.packages("rlang")
+
+#fungiguild analyses
+if(!requireNamespace("BiocManager")){
+  install.packages("BiocManager")}
+BiocManager::install("phyloseq")
